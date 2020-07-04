72a1ebcae4c7ddcfd0ae2bfc170a07e9acaabd31
Medet <b.m.bakytzhan@gmail.com>
2020-07-05 01:02:08 +0600

    code css

--- resources/views/mail/layouts/master.blade.php
+++ resources/views/mail/layouts/master.blade.php
@@ -11,8 +11,8 @@
 <div style="height: 100px; width: 100%; background-color: #2b6cb0; border-radius: 10px">
     <div style="height: 100%;text-align: center">
         <a href="{{ route('home') }}" class="inline-block">
-            <code style="height: 80%;"><img src="{{ asset('images/logos/logo_center_teddy.png') }}" alt=""
-                 style="height: 80%"></code>
+            <code>height: 80%</code><img src="{{ asset('images/logos/logo_center_teddy.png') }}" alt=""
+                 style="height: 80%">
         </a>
     </div>
 </div>
