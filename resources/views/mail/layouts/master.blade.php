248223a8db2bf6e64c95188139e92378460586d3
Medet <b.m.bakytzhan@gmail.com>
2020-07-05 01:00:49 +0600

    code

--- resources/views/mail/layouts/master.blade.php
+++ resources/views/mail/layouts/master.blade.php
@@ -11,7 +11,7 @@
 <div style="height: 100px; width: 100%; background-color: #2b6cb0; border-radius: 10px">
     <div style="height: 100%;text-align: center">
         <a href="{{ route('home') }}" class="inline-block">
-            <code><img src="{{ asset('images/logos/logo_center_teddy.png') }}" alt=""
+            <code style="height: 80%;"><img src="{{ asset('images/logos/logo_center_teddy.png') }}" alt=""
                  style="height: 80%"></code>
         </a>
     </div>
